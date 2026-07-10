309d361f2aec7d335015cfebbe0f20225a550a84
himanshu-codebyme <dabodiyahimanshu123@gmail.com>
2026-07-11 00:20:38 +0530

    file: Adding basic topic files (#7)

---
+++ C++/Code/04.Variables.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // ===========================================================
+    //                        VARIABLES
+    // ===========================================================
+    // Variables is like container used for storeing various type of data.
+
+    // Variable Declaration
+    // -----------------------------------------------------------
+    int age;
+    float salary;
+    char grade;
+    bool isStudent;
+
+    // Variable Initialization
+    // -----------------------------------------------------------
+    int marks = 95;
+    float pi = 3.14f;
+    char section = 'A';
+    bool isPassed = true;
+
+    // Variable Assignment
+    // -----------------------------------------------------------
+    int number;
+    number = 100;
+
+    // Variable Reassignment
+    // -----------------------------------------------------------
+    int score = 80;
+    score = 90;
+    score = 100;
+
+    // Multiple Variable Declaration
+    // -----------------------------------------------------------
+    int x, y, z;
+
+    // Multiple Variable Initialization
+    // -----------------------------------------------------------
+    int a = 10, b = 20, c = 30;
+
+    // Copying Variable Value
+    // -----------------------------------------------------------
+    int first = 50;
+    int second = first;
+
+    // Valid Variable Names
+    // -----------------------------------------------------------
+    int studentAge;
+    int student_age;
+    int studentAge2026;
+    int _marks;
+
+    // -----------------------------------------------------------
+    // Invalid Variable Names (Examples)
+    // -----------------------------------------------------------
+
+    // int 2age;          // Cannot start with a number
+    // int student age;   // Spaces are not allowed
+    // int int;           // Keyword cannot be used
+
+    return 0;
+}
